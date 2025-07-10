50daa100ba496277af7ffa0e1143587bec02bac0
vagrant <vagrant@homestead>
2025-07-10 21:41:03 +0000

    update code

--- backend/src/community-categories/dto/create-community-category.dto.ts
+++ backend/src/community-categories/dto/create-community-category.dto.ts
@@ -1,11 +1,14 @@
+import { ApiProperty } from '@nestjs/swagger';
 import { IsString, IsNotEmpty } from 'class-validator';
 
 export class CreateCommunityCategoryDto {
   @IsString()
   @IsNotEmpty()
+  @ApiProperty({ example: 'abc' })
   name: string;
 
   @IsString()
   @IsNotEmpty()
+  @ApiProperty({ example: 'url' })
   image: string;
 }
